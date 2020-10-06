154d4487e037d38fe670a86cfa1a44163e720323
Gianlucca Vick <luccavick31@hotmail.com>
2020-10-06 14:17:02 -0300

    Primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import Tmdb from './Tmdb';
+import MovieRow from './components/MovieRow';
+import FeaturedMovie from './components/FeaturedMovie';
+import Header from './components/Header';
+
 import './App.css';
 
-function App() {
+export default function App() {
+  const [moveList, setMoveList] = useState([]);
+  const [featureData, setFeatureData] = useState(null);
+  const [blackHeader, setBlackHeader] = useState(false);
+  const [header, setHeader] = useState(false);
+  useEffect(() => {
+    loadAll();
+  }, [header]);
+
+  useEffect(() => {
+    const scrollListener = () => {
+      if (window.scrollY > 30) {
+        setBlackHeader(true);
+      } else {
+        setBlackHeader(false);
+      }
+    };
+    window.addEventListener('scroll', scrollListener);
+
+    return () => {
+      window.removeEventListener('scroll', scrollListener);
+    };
+  }, []);
+
+  const loadAll = async () => {
+    const list = await Tmdb.getHomeList();
+    setMoveList(list);
+
+    const originals = list.filter((item) => item.slug === 'originals') || [];
+    if (originals.length && originals[0].items.results) {
+      const random = Math.floor(
+        Math.random() * (originals[0].items.results.length - 1)
+      );
+      const original = originals[0].items.results[random];
+      const originalDetail = await Tmdb.getMovieDetails(original.id, 'tv');
+      setFeatureData(originalDetail);
+    }
+  };
+
+  const userClick = (e) => {
+    e.preventDefault();
+    setHeader(!header);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='page'>
+      <Header header={header} black={blackHeader} click={userClick} />
+      {featureData && <FeaturedMovie data={featureData} />}
+      <section className='lists'>
+        {moveList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items} />
+        ))}
+      </section>
+
+      <footer>
+        Curso Youtube B7web
+        <br />
+        Direitos de imagem para Netflix
+        <br />
+        Dados consumidos so site Themoviedb.org
+      </footer>
+
+      {moveList.length <= 0 && (
+        <div className='loading'>
+          <img
+            src={`https://www.filmelier.com/pt/br/news/wp-content/uploads/2020/03/netflix-loading.gif`}
+            alt='loading'
+          />
+        </div>
+      )}
     </div>
   );
 }
-
-export default App;
